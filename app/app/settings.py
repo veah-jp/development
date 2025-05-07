7b2ef205aaf53645f97fd87167591940ae5c6de5
Justin Nadela <justin.nadela@urios.edu.ph>
2025-05-07 05:13:17 +0000

    sa client ni

--- app/app/settings.py
+++ app/app/settings.py
@@ -52,7 +52,7 @@ CORS_URLS_REGEX = r'^/api/.*$'
 
 # Add media types to CORS
 CORS_ALLOWED_ORIGINS = [
-    "http://172.17.100.14:3353",
+    "http://172.17.100.14:3383",
 ]
 
 # Application definition
@@ -135,9 +135,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Use MySQL engine
-        'NAME': 'bugasia6_db1',                 # Your database name
-        'USER': 'bugasia6',                     # Your MySQL username
-        'PASSWORD': '123456',                  # Your MySQL password
+        'NAME': 'default_db1',                 # Your database name
+        'USER': 'default',                     # Your MySQL username
+        'PASSWORD': '1234',                  # Your MySQL password
         'HOST': 'localhost',                   # MySQL server host (localhost if on the same machine)
         'PORT': '3321',                        # MySQL default port
     }
